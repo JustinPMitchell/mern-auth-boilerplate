f72ebf4c58358b4015a28d56c75cf7df9f93a29f
JustinPMitchell <j.mitchell.real@gmail.com>
2018-01-29 15:57:32 -0800

    removed default react

--- models/user.js
+++ models/user.js
@@ -23,19 +23,16 @@ var userSchema = new mongoose.Schema({
   },
   height: {
     type: String,
-    required: true,
     minlength: 8,
     maxlength: 99
   },
   weight: {
     type: String,
-    required: true,
     minlength: 8,
     maxlength: 99
   },   
   gender: {
     type: String,
-    required: true,
     minlength: 8,
     maxlength: 99
   }
