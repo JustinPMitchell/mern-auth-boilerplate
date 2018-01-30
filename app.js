f76363e0d85573520fb0ee00baedd161305e2928
JustinPMitchell <j.mitchell.real@gmail.com>
2018-01-30 14:46:09 -0800

    updated settings

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(function(req, res, next) {
 // Controllers
 app.use('/auth', require('./routes/auth'));
 app.use('/api/meal', meal);
+app.use('/settings', require('./routes/settings'));
 
 app.get('*', function(req, res, next) {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
